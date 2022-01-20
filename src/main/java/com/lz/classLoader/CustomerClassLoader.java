4082b41b25bd2b862cb161b73efad603b905aa58
连长 <1150429850@qq.com>
2022-01-20 17:20:00 +0800

    自定义类加载器

---
+++ src/main/java/com/lz/classLoader/CustomerClassLoader.java
@@ -0,0 +1,57 @@
+package com.lz.classLoader;
+
+import java.io.*;
+
+public class CustomerClassLoader extends ClassLoader{
+
+    private String byteCodePath;
+
+    public CustomerClassLoader(String byteCodePath) {
+        this.byteCodePath = byteCodePath;
+    }
+
+    public CustomerClassLoader(ClassLoader parent, String byteCodePath) {
+        super(parent);
+        this.byteCodePath = byteCodePath;
+    }
+
+    @Override
+    protected Class<?> findClass(String className) throws ClassNotFoundException {
+        //获取字节码完整路径
+        String fileName = byteCodePath + className + ".class";
+        ByteArrayOutputStream baos = null;
+        BufferedInputStream bis = null;
+        try {
+            //获取输入流
+            bis = new BufferedInputStream(new FileInputStream(fileName));
+            //获取输出流
+            baos = new ByteArrayOutputStream();
+            int len;
+            byte[] data = new byte[1024];
+            //读取数据并写出
+            while ((len = bis.read(data)) != -1) {
+                baos.write(data, 0, len);
+            }
+            //获取内存中完整的字节数组的数据
+            byte[] byteCodes = baos.toByteArray();
+            //将字节数组的数据转换为Class的实例
+            Class clazz = defineClass(null, byteCodes, 0, byteCodes.length);
+
+            return clazz;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null)
+                    baos.close();
+                if (bis != null)
+                    bis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/lz/classLoader/CustomerClassLoaderTest.java
@@ -0,0 +1,17 @@
+package com.lz.classLoader;
+
+public class CustomerClassLoaderTest {
+
+    public static void main(String[] args) {
+        CustomerClassLoader loader = new CustomerClassLoader("D:/");
+        try {
+            Class clazz = loader.loadClass("Case1");
+            System.out.println("加载此类的类加载器为："+clazz.getClassLoader().getClass().getName());
+            System.out.println("加载此类父类的类加载器为："+clazz.getClassLoader().getParent().getClass().getName());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
